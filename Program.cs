907e4b7d24bb0fa87aa3f2332e3cf055daa3820f
Attben <mrtobone@gmail.com>
2020-11-06 16:55:22 +0100

    Added path to data file, expanded code skeletons.

--- Program.cs
+++ Program.cs
@@ -19,15 +19,20 @@ namespace AddressBook
     {
         static List<Person> LoadAddressBook(string srcFilePath)
         {
-
+            List<Person> addressBook = new List<Person>();
+            //Logic not yet implemented
+            return addressBook;
         }
 
         static void SaveAddressBook(List<Person> data, string destFilePath)
         {
-
+            //NYI
         }
-        static void Main(string[] args)
+        static void Main()
         {
+            //Source: https://stackoverflow.com/questions/38075381/get-documents-folder-path-of-current-logged-on-user
+            string addressBookPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\AddressBook.txt";
+            List<Person> addressBook = LoadAddressBook(addressBookPath);
         }
     }
 }
