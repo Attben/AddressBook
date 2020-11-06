00eee863f9c5d89b64e0c238a9f27b60f864014a
Attben <mrtobone@gmail.com>
2020-11-06 16:18:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Person
+    {
+        string name, address, phone, email;
+
+        public Person(string name, string address, string phone, string email)
+        {
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.email = email;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
